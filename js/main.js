851f252cd16b7db0c3025c891d489adf42127182
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-07-04 19:23:24 +0200

    add: math round

--- js/main.js
+++ js/main.js
@@ -13,11 +13,11 @@ const annoCorrente = adesso.getFullYear();
 const eta = annoCorrente - annoNascita;
 
 if ( (eta >= 18) && (eta <= 65) ) {
-    alert(`Il prezzo del tuo biglietto è: ${costoTotaleXKm}`);
+    alert(`Il prezzo del tuo biglietto è: ${Math.round((costoTotaleXKm) * 100) / 100}`);
 } else if ( (eta >= 0) && (eta < 18) ) {
-    alert(`Il prezzo del tuo biglietto è: ${(costoTotaleXKm - costoTotaleXKm*20/100)}`);
+    alert(`Il prezzo del tuo biglietto è: ${Math.round((costoTotaleXKm - costoTotaleXKm*20/100) * 100) / 100}`);
 } else if (eta > 65) {
-    alert(`Il prezzo del tuo biglietto è: ${(costoTotaleXKm - costoTotaleXKm*40/100)}`);
-} else if (eta < 0) {
-    alert(`Il prezzo del tuo biglietto è: Devi ancora nascere`);
+    alert(`Il prezzo del tuo biglietto è: ${Math.round((costoTotaleXKm - costoTotaleXKm*40/100) * 100) / 100}`);
+} else {
+    alert(`Devi ancora nascere!`);
 }
